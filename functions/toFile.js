999ac917fbd22efa298ab6799d8715aed8929bc7
aIhamdulillah <118680146+aIhamdulillah@users.noreply.github.com>
2023-05-09 01:57:48 +0800

    Update toFile.js

--- functions/toFile.js
+++ functions/toFile.js
@@ -1,23 +1,13 @@
 const fs = require("fs");
 const filePath = "accounts.json";
 
-/**
- * This file exports two functions for writing and retrieving account information from a JSON file.
+/** 
  * @function writeToFile
- * @description Adds a new account and its 2FA key to an existing JSON file or creates a new file if none exists. Returns a Promise that resolves when the operation is successful, and rejects with an error if there's a problem.
+ * @description Adds a new account and its 2FA key to an existing JSON file, or creates a new file if none exists.
  * @param {string} account - The name of the account being added.
  * @param {string} key - The 2FA key associated with the account.
- * @returns {Promise<void>}
- * 
- * @function removeFromFile
- * @description Removes an account and its 2FA key from an existing JSON file. Returns a Promise that resolves when the operation is successful, and rejects with an error if the account is not found or there's a problem with the file.
- * @param {string} account - The name of the account being removed.
- * @returns {Promise<void>}
- *
- * @function getAllCodes
- * @description Retrieves all accounts and their associated 2FA keys from an existing JSON file, if one exists. Returns a JSON object containing all accounts and their associated 2FA keys.
- * @returns {Object}
- */
+ * @returns {void}
+*/
 
 const writeToFile = (account, key) => {
     return new Promise((resolve, reject) => {
@@ -43,6 +33,12 @@ const writeToFile = (account, key) => {
     });
 };
 
+/**
+ * @function removeFromFile
+ * @description Removes an account and its 2FA key from an existing JSON file.
+ * @param {string} account - The name of the account being removed.
+ * @returns {void}
+ */
 const removeFromFile = (account) => {
     return new Promise((resolve, reject) => {
         // Load existing data, if any
@@ -73,6 +69,11 @@ const removeFromFile = (account) => {
     });
 };
 
+/**
+ * @function getAllCodes
+ * @description Retrieves all accounts and their associated 2FA keys from an existing JSON file, if one exists.
+ * @returns {Object} Returns a JSON object containing all accounts and their associated 2FA keys.
+ */
 const getAllCodes = () => {
     // Read data from the file, if it exists
     let data = {};
